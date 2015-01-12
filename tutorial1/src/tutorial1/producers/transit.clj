9036df7993a97ca70c857d7a2ec855087f71fd07
Malcolm Lockyer <mlockyer@liveops.com>
2015-01-12 14:52:40 +1300

    complete simple transit implementation
    basically exactly the same as the file producer, time to add some more
    stuff :)

--- tutorial1/src/tutorial1/producers/transit.clj
+++ tutorial1/src/tutorial1/producers/transit.clj
@@ -2,5 +2,46 @@
   (:require [schema.core :as s]
             [clojure.java.io :as io]
             [clojure.string :as str]
-            [cognitect.transit :as transit]))
+            [cognitect.transit :as transit])
+  (:import [java.io FileInputStream FileOutputStream]))
 
+(defn file-exists?
+  [file]
+  (.exists (io/as-file file)))
+
+(defn read-quips
+  [file]
+  (if (file-exists? file)
+    (let [in (FileInputStream. file)
+          reader (transit/reader in :msgpack)]
+      (:quips (transit/read reader)))
+    []))
+
+(defn write-quips
+  [file quips]
+  (let [out (FileOutputStream. file false)
+        writer (transit/writer out :msgpack)]
+    (transit/write writer {:quips quips})))
+
+(defn add-quip
+  [file quip]
+  (write-quips file (conj (read-quips file) quip)))
+
+(defn get-quip
+  [file]
+  (if (file-exists? file)
+    (rand-nth (read-quips file))
+    nil))
+
+(defn all-quips
+  [file]
+  (read-quips file))
+
+(defn count-quips
+  [file]
+  (count (read-quips file)))
+
+(defn drop-quips
+  [file]
+  (if (file-exists? file)
+    (io/delete-file file true)))
--- tutorial1/test/tutorial1/producers/transit_test.clj
+++ tutorial1/test/tutorial1/producers/transit_test.clj
@@ -8,7 +8,6 @@
 (def quip "If at first you don't succeed... so much for skydiving.")
 (def mquip (str "First the doctor told me the good news: \n"
                 "I was going to have a disease named after me."))
-(def emquip (file/escape-newlines mquip))
 
 (defn delete-test-file
   "Delete the test file silently"
@@ -27,37 +26,36 @@
 (deftest add-quip
   []
   (t/add-quip test-file quip)
-  (is (= (slurp test-file) (str/join [quip "\n"]))))
+  (is (and (= (t/count-quips test-file) 1)
+           (= (t/get-quip test-file) quip))))
 
 (deftest add-2-quips
   []
   (dotimes [n 2] (t/add-quip test-file quip))
-  (is (= (slurp test-file) (str/join [quip "\n" quip "\n"]))))
+  (is (and (= (t/count-quips test-file) 2)
+           (= (t/get-quip test-file) quip))))
 
 (deftest add-multiline-quip
   []
   (t/add-quip test-file mquip)
-  (is (= (slurp test-file) (str/join [emquip "\n"]))))
+  (is (and (= (t/count-quips test-file) 1)
+           (= (t/get-quip test-file) mquip))))
 
 (deftest add-2-multiline-quips
   []
   (dotimes [n 2] (t/add-quip test-file mquip))
-  (is (= (slurp test-file) (str/join [emquip "\n" emquip "\n"]))))
+  (is (and (= (t/count-quips test-file) 2)
+           (= (t/get-quip test-file) mquip))))
 
 (deftest get-quip
   []
   (t/add-quip test-file quip)
-  (is (= (file/get-quip test-file) quip)))
+  (is (= (t/get-quip test-file) quip)))
 
 (deftest get-all-quips
   []
-  (spit test-file (str/join [emquip "\n" emquip "\n"]))
-  (is (= (t/all-quips test-file) [mquip mquip])))
-
-(deftest get-multiline-quip
-  []
-  (file/add-quip test-file mquip)
-  (is (= (t/get-quip test-file) mquip)))
+  (dotimes [n 42] (t/add-quip test-file mquip))
+  (is (= (t/all-quips test-file) (take 42 (repeat mquip)))))
 
 (deftest drop-quips
   []
