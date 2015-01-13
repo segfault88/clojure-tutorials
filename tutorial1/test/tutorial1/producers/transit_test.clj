ca574dc80dedc89a4ea0d769a745eca87791cf40
Malcolm Lockyer <segfault88@gmail.com>
2015-01-13 18:51:59 +1300

    fix transit test

--- tutorial1/test/tutorial1/producers/transit_test.clj
+++ tutorial1/test/tutorial1/producers/transit_test.clj
@@ -2,12 +2,15 @@
   (:require [clojure.test :refer :all]
             [clojure.java.io :as io]
             [clojure.string :as str]
-            [tutorial1.producers.transit :as t]))
+            [tutorial1.producers.producer :as p]
+            [tutorial1.producers.transit :as transit]))
 
 (def test-file "quipper_test_file.dat")
 (def quip "If at first you don't succeed... so much for skydiving.")
 (def mquip (str "First the doctor told me the good news: \n"
                 "I was going to have a disease named after me."))
+
+(def t (tutorial1.producers.transit.TransitProducer. test-file))
 
 (defn delete-test-file
   "Delete the test file silently"
@@ -25,45 +28,45 @@
 
 (deftest add-quip
   []
-  (t/add-quip test-file quip)
-  (is (and (= (t/count-quips test-file) 1)
-           (= (t/get-quip test-file) quip))))
+  (p/add-quip t quip)
+  (is (and (= (p/count-quips t) 1)
+           (= (p/get-quip t) quip))))
 
 (deftest add-2-quips
   []
-  (dotimes [n 2] (t/add-quip test-file quip))
-  (is (and (= (t/count-quips test-file) 2)
-           (= (t/get-quip test-file) quip))))
+  (dotimes [n 2] (p/add-quip t quip))
+  (is (and (= (p/count-quips t) 2)
+           (= (p/get-quip t) quip))))
 
 (deftest add-multiline-quip
   []
-  (t/add-quip test-file mquip)
-  (is (and (= (t/count-quips test-file) 1)
-           (= (t/get-quip test-file) mquip))))
+  (p/add-quip t mquip)
+  (is (and (= (p/count-quips t) 1)
+           (= (p/get-quip t) mquip))))
 
 (deftest add-2-multiline-quips
   []
-  (dotimes [n 2] (t/add-quip test-file mquip))
-  (is (and (= (t/count-quips test-file) 2)
-           (= (t/get-quip test-file) mquip))))
+  (dotimes [n 2] (p/add-quip t mquip))
+  (is (and (= (p/count-quips t) 2)
+           (= (p/get-quip t) mquip))))
 
 (deftest get-quip
   []
-  (t/add-quip test-file quip)
-  (is (= (t/get-quip test-file) quip)))
+  (p/add-quip t quip)
+  (is (= (p/get-quip t) quip)))
 
 (deftest get-all-quips
   []
-  (dotimes [n 42] (t/add-quip test-file mquip))
-  (is (= (t/all-quips test-file) (take 42 (repeat mquip)))))
+  (dotimes [n 42] (p/add-quip t mquip))
+  (is (= (p/all-quips t) (take 42 (repeat mquip)))))
 
 (deftest drop-quips
   []
   (spit test-file "there is stuff here")
-  (t/drop-quips test-file)
+  (p/drop-quips t)
   (is (= (io/delete-file test-file :silent) :silent)))
 
 (deftest count-quips
   []
-  (dotimes [n 42] (t/add-quip test-file mquip))
-  (is (= (t/count-quips test-file) 42)))
+  (dotimes [n 42] (p/add-quip t mquip))
+  (is (= (p/count-quips t) 42)))
